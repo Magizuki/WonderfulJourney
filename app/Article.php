bedc4540d1c09e068b1eaf126568edf5a9a44e48
Magizuki <irvinesenjaya@yahoo.co.id>
2021-02-13 17:53:05 +0700

    Eloquent Model for Database Done

---
+++ app/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $table = 'articles';
+
+    protected $fillable = ['id', 'user_id', 'category_id', 'title', 'description', 'image'];
+
+    public function User(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function Category(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,38 +2,20 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $table = 'users';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    protected $fillable = ['name', 'email', 'phone', 'role'];
+
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token'
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function Article(){
+        return $this->hasMany(Article::class);
+    }
 }
